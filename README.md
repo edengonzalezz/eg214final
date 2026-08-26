b19862ec6f42e29c8e069f808879fcc13ad62e95
edengonzalezz <edengonzalez@ucsb.edu>
2026-08-26 13:33:52 -0700

    updated spaghetti

---
+++ README.md
@@ -0,0 +1,25 @@
+# Ion concentrations in Puerto Rico streams before and after Hurricane Hugo.
+
+
+
+## Purpose
+R code for the replication of Figure 3 of the paper by Schaefer et al. (2000), which graphs the concentration of five different ions in different streams in Puerto Rico. The graph places year on the x-axis (1988-1994) facets by ion (potassium, nitrate, magnesium, calcium, and amonium) and plots the 9-week moving average concentration by four stream (Puente Roto Mameyes, Bisley Quebrada 1, Bisley Quebrada 2, Bisley Quebrada 3).
+
+
+
+## What is in the repo?
+
+The GitHub reprository has the following structure:
+* /data contains the downloaded zip file. 
+* /analyses
+
+
+## Data access
+
+The .zip file containing the data provided by the Environmental Data Initiative were downloaded from here: McDowell and International Institute Of Tropical Forestry (IITF) 2024.
+
+## Contributors
+
+## References 
+
+Schaefer, Douglas. A., William H. McDowell, Fredrick N. Scatena, and Clyde E. Asbury. 2000. “Effects of Hurricane Disturbance on Stream Water Concentrations and Fluxes in Eight Tropical Forest Watersheds of the Luquillo Experimental Forest, Puerto Rico.” Journal of Tropical Ecology 16 (2): 189–207. https://doi.org/10.1017/s0266467400001358.
--- scratch/spaghetti.R
+++ scratch/spaghetti.R
@@ -89,3 +89,154 @@ ggplot(bq3_longer, mapping =
   geom_line() +
   facet_wrap(~ions, scales = "free", ncol = 1) +
   theme_bw()
+
+
+
+
+
+# try to do moving average -----------------------------------------------
+
+
+# 9 week average  
+
+window_start <- seq(ymd("1988-01-01"), ymd("1994-12-31"), by = "9 weeks")
+window_end <- window_start + weeks(9)
+
+prm_smoothed <- tibble(
+  window_start,
+  window_end,
+  mean_k = NA,
+  mean_mg = NA,
+  mean_no3 = NA,
+  mean_ca = NA,
+  mean_nh4 = NA
+)
+prm_smoothed
+
+
+
+for (i in 1:nrow(prm_smoothed)) {
+  # i is our iterator
+  # 1:nrow(qs_smoothed) is our sequence
+  # i will take on those values, one at a time
+
+  
+  start_date <- window_start[i]
+  end_date <- window_end[i]
+
+  # what potassium values are inside that window?
+  sample_k <- prm_sub$K[
+    start_date <= prm_sub$Sample_Date &
+      end_date > prm_sub$Sample_Date
+  ]
+  # what's the mean?
+  mean_k <- mean(sample_k, na.rm = TRUE)
+  
+  
+  # magnesium values
+  mean_mg <- mean(
+    prm_sub$Mg[
+      start_date <= prm_sub$Sample_Date &
+        end_date > prm_sub$Sample_Date
+    ],
+    na.rm = TRUE
+  )
+
+  # calcium 
+  mean_ca <- mean(
+    prm_sub$Ca[
+      start_date <= prm_sub$Sample_Date &
+        end_date > prm_sub$Sample_Date
+    ],
+    na.rm = TRUE
+  )
+
+  # nitrate
+  mean_no3 <- mean(
+   prm_sub$`NO3-N`[
+     start_date <= prm_sub$Sample_Date &
+       end_date > prm_sub$Sample_Date
+    ],
+    na.rm = TRUE
+  )
+
+  # ammonium 
+  mean_nh4 <- mean(
+   prm_sub$`NH4-N`[
+     start_date <= prm_sub$Sample_Date &
+       end_date > prm_sub$Sample_Date
+    ],
+    na.rm = TRUE
+  )
+
+
+  # how do you put it in the result?
+  
+
+  prm_smoothed$mean_k[i] <- mean_k
+  prm_smoothed$mean_mg[i] <- mean_mg
+  prm_smoothed$mean_ca[i] <- mean_ca
+  prm_smoothed$mean_no3[i] <- mean_no3
+  prm_smoothed$mean_nh4[i] <- mean_nh4
+
+
+}
+
+tail(prm_smoothed)
+
+
+
+
+
+# try to graph moving average --------------------------------------------
+
+
+prm_pivot <- prm_smoothed |>
+  pivot_longer(
+    cols = c(mean_k, mean_mg, mean_no3, mean_ca, mean_nh4),
+    names_to = "ion",
+    values_to = "mean_concentration"
+  )
+tail(prm_pivot)
+
+ggplot(prm_pivot, mapping = 
+  aes(x = window_start, y = mean_concentration, color = ion) ) +
+  geom_line() +
+  facet_wrap(~ion, scales = "free", ncol = 1) +
+  theme_bw()
+
+
+
+
+
+
+
+
+
+
+
+
+
+# safety blanket code ----------------------------------------------------
+
+
+
+qs_pivot <- qs_smoothed |>
+  pivot_longer(
+    cols = c(mean_k, mean_mg),
+    names_to = "ion",
+    values_to = "mean_concentration"
+  )
+
+
+ggplot(
+  data = qs_pivot,
+  mapping = aes(x = window_start, y = mean_concentration, color = ion)
+) +
+  geom_point() +
+  labs(
+    x = "Date",
+    y = " Mean Concentration",
+    title = "Moving average of K and Mg"
+  ) +
+  theme_bw()
